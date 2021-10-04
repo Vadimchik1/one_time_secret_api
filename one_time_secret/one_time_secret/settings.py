bd3f3bb439d954f97862d7f5c64d501b7299cc15
Vadim Ganitsa <vadimganica@gmail.com>
2021-10-04 23:21:03 +0600

    Database was changed on posgresql and app was packed in docker containers

--- one_time_secret/one_time_secret/settings.py
+++ one_time_secret/one_time_secret/settings.py
@@ -75,10 +75,21 @@ WSGI_APPLICATION = 'one_time_secret.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+#
+# }
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'secrets',
+        'USER': 'admin',
+        'PASSWORD': 'admin',
+        'HOST': 'database',
+        'PORT': '5432'
     }
 }
 
